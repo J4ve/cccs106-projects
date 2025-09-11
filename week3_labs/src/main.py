4fc4b1917cbb4a7bd26be41e1325604957d06a2e
J4ve <153438738+J4ve@users.noreply.github.com>
2025-09-12 02:25:41 +0800

    Add db_connection.py in week3_labs/src, edited requirements.txt, renamed week3_labs flet project name to userlogin, edited main.py

--- week3_labs/src/main.py
+++ week3_labs/src/main.py
@@ -1,26 +1,62 @@
 import flet as ft
+import mysql.connector
+from db_connection import connect_db
+
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    page.title = "User Login"
+    page.theme_mode=ft.ThemeMode.LIGHT  # force to light mode to fix dark mode bad ui
+    page.window.frameless = True
+    page.window.height = 350
+    page.window.width = 400
+    page.bgcolor = ft.Colors.AMBER_ACCENT
+    page.window.center()
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    def button_clicked(e):
+        page.update()
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+
     page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
+        ft.Text(
+                    "User Login",
+                    size=20, 
+                    weight=ft.FontWeight.BOLD
+                ),
+        ft.TextField(
+                        label="Username", 
+                        hint_text="Enter your username",
+                        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+                    ),
+        ft.TextField(
+                        label="Password", 
+                        hint_text="Enter your password",
+                        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+                    ),
+        ft.ElevatedButton(text="Submit", on_click=button_clicked),
     )
+
+    # counter = ft.Text("0", size=50, data=0)
+
+    # def increment_click(e):
+    #     counter.data += 1
+    #     counter.value = str(counter.data)
+    #     counter.update()
+
+    # page.floating_action_button = ft.FloatingActionButton(
+    #     icon=ft.Icons.ADD, on_click=increment_click
+    # )
+    # page.add(
+    #     ft.SafeArea(
+    #         ft.Container(
+    #             counter,
+    #             alignment=ft.alignment.center,
+    #         ),
+    #         expand=True,
+    #     )
+    # )
 
 
 ft.app(main)
