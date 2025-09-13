760baee2c9ba83f2fa888cee76793b8e63e457c7
J4ve <153438738+J4ve@users.noreply.github.com>
2025-09-14 05:49:16 +0800

    Added async function for clicking submit, dialogs still in login_click function

--- week3_labs/src/main.py
+++ week3_labs/src/main.py
@@ -2,8 +2,6 @@ import flet as ft
 import mysql.connector
 from db_connection import connect_db
 
-
-
 def main(page: ft.Page):
     page.title = "User Login"
     page.theme_mode=ft.ThemeMode.LIGHT  # force to light mode to fix dark mode bad ui
@@ -15,48 +13,56 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    def button_clicked(e):
+    username = ft.TextField(
+                    label="Username", 
+                    hint_text="Enter your username",
+                    bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+                )
+    password = ft.TextField(
+                    label="Password", 
+                    hint_text="Enter your password",
+                    bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+                )
+    
+    async def login_click(e):
+
+        
+        success_dialog = ft.AlertDialog(title=f"Welcome, {username.value}",
+                                        
+                                        )
+        failure_dialog = ft.AlertDialog(title="Login Failed",
+                                        
+                                        )
+        invalid_input_dialog = ft.AlertDialog(title="Input Error",
+                                              content=ft.Text("Please enter username and password"),
+                                              alignment=ft.alignment.center,
+                                              actions=[
+                                                  ft.TextButton("OK", on_click=lambda e: page.close(invalid_input_dialog))
+                                              ]
+                                              )
+        database_error_dialog = ft.AlertDialog(title="Database Error",
+                                               
+                                               )
+        
+        print("clicked") # test ko lang if gumagana yung pag click ng submit
+
+        if username.value and password.value != "":
+            connect_db()
+        else:
+            page.open(invalid_input_dialog)
+
         page.update()
 
 
     page.add(
         ft.Text(
-                    "User Login",
-                    size=20, 
-                    weight=ft.FontWeight.BOLD
-                ),
-        ft.TextField(
-                        label="Username", 
-                        hint_text="Enter your username",
-                        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
-                    ),
-        ft.TextField(
-                        label="Password", 
-                        hint_text="Enter your password",
-                        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
-                    ),
-        ft.ElevatedButton(text="Submit", on_click=button_clicked),
+            "User Login",
+            size=20, 
+            weight=ft.FontWeight.BOLD
+        ),
+        username,
+        password,
+        ft.ElevatedButton(text="Submit", on_click=login_click),
     )
-
-    # counter = ft.Text("0", size=50, data=0)
-
-    # def increment_click(e):
-    #     counter.data += 1
-    #     counter.value = str(counter.data)
-    #     counter.update()
-
-    # page.floating_action_button = ft.FloatingActionButton(
-    #     icon=ft.Icons.ADD, on_click=increment_click
-    # )
-    # page.add(
-    #     ft.SafeArea(
-    #         ft.Container(
-    #             counter,
-    #             alignment=ft.alignment.center,
-    #         ),
-    #         expand=True,
-    #     )
-    # )
-
 
 ft.app(main)
